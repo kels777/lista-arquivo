df78e40c3c6972da42b1826dd23ce6b36ac3a298
Kaio Eduardo <kaioeduardo2211@gmail.com>
2025-07-10 13:21:37 -0300

    segundo

--- ex7.c
+++ ex7.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    FILE *atletas;
-    atletas = fopen("c:\\Users\\kaioe\\OneDrive\\Documentos\\projetos\\arquivos\\lista de atletas.txt","r");
-    if(atletas == NULL){
-        puts("Falha ao abrir o arquivo!!");
-        exit(1);
+typedef struct {
+    char nome[50];
+    int idade;
+    float altura;
+} Atleta;
+
+int main() {
+    FILE *arquivo;
+    Atleta atletas[5];
+
+    arquivo = fopen("atletas.bin", "rb");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
     }
 
-    char linha [100];
-    for(int i = 0; i < 5;i++){
-        for(int j = 0;j < 5;j++){
-            fgets(linha,sizeof(linha),atletas);
-            printf("%s",linha);
+    fread(atletas, sizeof(Atleta), 5, arquivo);
+    fclose(arquivo);
+
+    int indiceMaisAlto = 0;
+    int indiceMaisVelho = 0;
+
+    for (int i = 1; i < 5; i++) {
+        if (atletas[i].altura > atletas[indiceMaisAlto].altura) {
+            indiceMaisAlto = i;
         }
-        printf("\n");
+        if (atletas[i].idade > atletas[indiceMaisVelho].idade) {
+            indiceMaisVelho = i;
+        }
     }
 
-    fclose(atletas);
+    printf("Atleta mais alto: %s\n", atletas[indiceMaisAlto].nome);
+    printf("Atleta mais velho: %s\n", atletas[indiceMaisVelho].nome);
+
     return 0;
 }
